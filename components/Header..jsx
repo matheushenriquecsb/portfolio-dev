9283935573bb43fbcf88ba8a503ab60c24ffc466
matheushenriquecsb <matheus.mhg2@gmail.com>
2024-10-31 22:16:57 -0300

    create setup and navbar

---
+++ components/Header..jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+import { Nav } from "./Nav";
+import { Button } from "./ui/button";
+
+export const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href={"/"}>
+          <h1 className="text-4xl font-semibold">
+            Matheus <span className="text-accent">.</span>
+          </h1>
+        </Link>
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href={""}>
+            <Button>Hire Me</Button>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
